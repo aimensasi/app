56a6b0063031ae000a827b4f1f2325b2e878ed51
AIMEN .S.A.SASI <aimensasi@icloud.com>
2020-09-12 22:16:04 +0800

    work in progress

--- resources/views/common/welcome.blade.php
+++ resources/views/common/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 
 @section('main')
-	<h1 class="text-6xl text-center font-thin text-gray-600">{{ config('app.name') }}</h1>
+	<h1 class="text-6xl font-thin text-center text-gray-600">{{ config('app.name') }}</h1>
+
+	<h1 class="mt-10 text-6xl font-thin text-center text-gray-600">SQL => {{ env('APP_ENV') }} - {{ env('DATABASE_CONNECTION') }} - {{ config('database.default') }}</h1>
 @endsection
